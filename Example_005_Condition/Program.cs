8aa7933160120147ebec9c8509e4a21d35c1f968
Nikita Zakharov <ii3axapobii@gmail.com>
2023-03-27 21:40:33 +0300

    folder Example_004_Condition added

---
+++ Example_005_Condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Tap username:");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "maria")
+{
+    Console.WriteLine("Wonderful!");
+}
+else
+{
+    Console.Write("Hellow, ");
+    Console.WriteLine(username);
+}
